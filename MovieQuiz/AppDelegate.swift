97c4e16934808b7d951454a33a33038e0dac9174
Andrei Zharov <zharovandrey1989@gmail.com>
2023-12-15 21:43:46 +0300

    Выполнил задания по 4 спринту: 1. Реализовал верстку по макету. 2. Разработал логику приложения по макету.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -4,8 +4,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
